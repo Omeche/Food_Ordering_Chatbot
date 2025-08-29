ec740d43a471bafc675984b333085742b31ae64a
Omeche <omechetochi@gmail.com>
2025-08-29 22:34:13 +0100

    Removed files

--- app/backend/application.py
+++
@@ -1,8 +0,0 @@
-# Import Flask app from main.py
-from main import app
-
-# Expose the Flask app as "application" for Elastic Beanstalk
-application = app
-
-if __name__ == "__main__":
-    application.run()
